8dbf533cb2bb619284e448d2af89c0bc54c08d97
Skywalkerkan <cosarerkann>
2024-06-14 22:50:13 +0300

    Finito

--- TurkcellFinalUITests/TurkcellFinalUITests.swift
+++ TurkcellFinalUITests/TurkcellFinalUITests.swift
@@ -27,41 +27,32 @@ final class TurkcellFinalUITests: XCTestCase {
     func test_pages() {
         
         let homeViewIdentifier = app.otherElements["HomeViewController"]
-        guard homeViewIdentifier.exists else {
-            return
-        }
+            guard homeViewIdentifier.waitForExistence(timeout: 5) else {
+                return
+            }
         
-        let tablesQuery = app.tables
-        let specificCell = tablesQuery.cells.containing(.staticText, identifier: "YourSpecificText").firstMatch
-        
-        if specificCell.exists {
-            specificCell.tap()
-        }
+         let tablesQuery = app.tables
+         let byJohnIsmayAndDesireeRiosStaticText = tablesQuery.firstMatch
+         byJohnIsmayAndDesireeRiosStaticText.tap()
     }
-
-    func testKeyboardAndSearchButton() {
-        let app = XCUIApplication()
-        
-        let homeViewIdentifier = app.otherElements["HomeViewController"]
-        guard homeViewIdentifier.exists else {
-            return
-        }
-        
+    
+    func testKeyboardAndSearchButton(){
         let searchBarElement = app.otherElements["searchBar"]
+        
         XCTAssertTrue(searchBarElement.waitForExistence(timeout: 5), "Search bar should exist")
         
         let searchButton = app.buttons["SearchButton"]
         let searchButtonFrameBefore = searchButton.frame
         
         searchBarElement.tap()
-        
+                
         let keyboard = app.keyboards.element
         XCTAssertTrue(keyboard.waitForExistence(timeout: 5), "The keyboard should appear")
         
         print(searchButtonFrameBefore.origin.y, searchButton.frame.origin.y)
-        XCTAssertTrue(searchButtonFrameBefore.origin.y > searchButton.frame.origin.y, "The search button should move up when the keyboard appears")
+        
+        XCTAssertTrue(searchButtonFrameBefore.origin.y >= searchButton.frame.origin.y, "The search button should move up when the keyboard appears")
     }
-
     
     
 }
